eaac20e899300f6d6fded72e87d5a1e5b0a3e59b
Rapaka <vivekanand.rapaka@gmail.com>
2018-10-28 21:31:35 +0530

    Modified greeter.java by adding comment

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,7 +11,7 @@ public class Greeter {
   public Greeter() {
 
   }
-
+  //comment
   //TODO: Add javadoc comment
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
